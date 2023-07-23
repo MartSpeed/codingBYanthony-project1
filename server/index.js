cfa35a053f5ce913bb44a1352e381fcd739fe601
John Anthony <jmanthony.fullsail@gmail.com>
2023-07-23 08:50:55 -0500

    chore: first commit

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors());
+
+const spideyModels = [
+    "Spider-Man", 
+    "Mary Jane Watson", 
+    "Green Goblin", 
+    "Venom", 
+    "Doctor Octopus", 
+    "Black Cat", 
+    "Electro", 
+    "Sandman", 
+    "Lizard", 
+    "Rhino", 
+    "Mysterio", 
+    "Kraven the Hunter", 
+    "Vulture", 
+    "Carnage", 
+    "Scorpion", 
+    "Shocker", 
+    "Kingpin", 
+    "Prowler", 
+    "Silver Sable", 
+    "Tombstone", 
+    "Hammerhead", 
+    "Jackal", 
+    "Chameleon", 
+    "Black Widow"
+];
+
+app.get('/', (request, response) => [
+    response.json({
+        message: 'You friendly neighborhood Spider-Man! '
+    })
+])
+
+app.post('/spideySense', (request, response) => {
+    console.log(request.body)
+})
+
+app.listen(5000, () => {
+    console.log('Listening on http://localhost:5000')
+})
